f69359e868559b6ca21fd100b734c63f7f70a981
mrosales <eric.mrosales@udgvirtual.udg.mx>
2025-03-03 18:30:43 -0600

    3.2 Los prefabs

---
+++ Assets/Scripts/JohnMovement.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class JohnMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal=Input.GetAxisRaw("Horizontal");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+#3.2 Los prefabs 
+
 # 3.1. La escena principal y la estructura del personaje
