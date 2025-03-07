61b1f278cc2c543801327f25314d0a61aa076737
mrosales <eric.mrosales@udgvirtual.udg.mx>
2025-03-07 14:52:19 -0600

    3.3 Programación de mecánicas y físicas

--- Assets/Scripts/JohnMovement.cs
+++ Assets/Scripts/JohnMovement.cs
@@ -2,15 +2,44 @@ using UnityEngine;
 
 public class JohnMovement : MonoBehaviour
 {
+    public float Speed;
+    public float JumpForce;
+    
+    private Rigidbody2D Rigidbody2D;
+    private float Horizontal;
+    private bool Grounded;
+    private Animator Animator;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+         Rigidbody2D = GetComponent<Rigidbody2D>();
+         Animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal=Input.GetAxisRaw("Horizontal");
+        Horizontal = Input.GetAxisRaw("Horizontal");
+        
+        Animator.SetBool("running",Horizontal != 0.0F);
+        
+        //Debug.DrawRay(transform.position, Vector3.down * 0.1f, Color.red);
+        Grounded = Physics2D.Raycast(transform.position,Vector2.down,0.1f);
+        
+        if(Input.GetKeyDown(KeyCode.W) && Grounded)
+        {
+            Jump();
+        }
+    }
+    
+    private void FixedUpdate()
+    {
+        Rigidbody2D.linearVelocity = new Vector2(Horizontal * Speed, Rigidbody2D.linearVelocity.y);
+    }
+    
+    private void Jump()
+    {
+        Rigidbody2D.AddForce(Vector2.up * JumpForce);
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-#3.2 Los prefabs 
+# 3.3 Programación de mecánicas y físicas
+
+# 3.2 Los prefabs 
 
 # 3.1. La escena principal y la estructura del personaje
